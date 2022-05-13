aef5b879f01fc9dd0c116cce967f64307ed5bd87
Christopher <cmava730@gmail.com>
2022-05-13 23:06:28 +0400

    Carlist and images added

--- app/src/main/java/com/example/splashscreenandmore/BuyCar.java
+++ app/src/main/java/com/example/splashscreenandmore/BuyCar.java
@@ -11,24 +11,24 @@ public class BuyCar extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_buy_car);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         MyCarData[] myMovieData = new MyCarData[]{
-                new MyCarData("Avengers","2019 film",R.drawable.avenger),
-                new MyCarData("Venom","2018 film",R.drawable.venom),
-                new MyCarData("Batman Begins","2005 film",R.drawable.batman),
-                new MyCarData("Jumanji","2019 film",R.drawable.jumanji),
-                new MyCarData("Good Deeds","2012 film",R.drawable.good_deeds),
-                new MyCarData("Hulk","2003 film",R.drawable.hulk),
-                new MyCarData("Avatar","2009 film",R.drawable.avatar),
+                new MyCarData("Tesla","Electric", R.drawable.avenger),
+                new MyCarData("Nissan Qashqai","1500cc",R.drawable.venom),
+                new MyCarData("Ford Ranger","1800cc",R.drawable.jumanji),
+                new MyCarData("Suzuki","1000cc",R.drawable.good_deeds),
+                new MyCarData("Ford Focus","1600cc",R.drawable.hulk),
+                new MyCarData("Nissan 350z","2500cc",R.drawable.avatar),
         };
 
         MyMovieAdapter myMovieAdapter = new MyMovieAdapter(myMovieData,BuyCar.this);
         recyclerView.setAdapter(myMovieAdapter);
+
 
     }
 }
