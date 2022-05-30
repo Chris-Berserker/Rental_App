f8521fbba0b5e8d49504969db9a01f399399771c
Christopher <cmava730@gmail.com>
2022-05-30 22:50:46 +0400

    Minor Design Fixes

--- app/src/main/java/com/example/splashscreenandmore/Loading.java
+++ app/src/main/java/com/example/splashscreenandmore/Loading.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class Loading extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 1400;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
