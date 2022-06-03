1c8560ee7e2258e1bbcdc3197ecfed5c556a2487
Christopher <cmava730@gmail.com>
2022-06-03 16:09:54 +0400

    Buy Button Linked

--- app/src/main/java/com/example/splashscreenandmore/Dashboard.java
+++ app/src/main/java/com/example/splashscreenandmore/Dashboard.java
@@ -4,13 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity{
+    public Button buybutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+
+        buybutton = (Button) findViewById(R.id.buybutton);
+        buybutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Dashboard.this, BuyCar.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
